068435ddd6fefa9ed1a493c9ec269ac855dc7f93
Guille Gonzalez <gonzalezreal@icloud.com>
2017-07-11 10:22:18 +0200

    Initial Commit

---
+++ Markup/Markup.h
@@ -0,0 +1,19 @@
+//
+//  Markup.h
+//  Markup
+//
+//  Created by Guille Gonzalez on 11/07/2017.
+//  Copyright © 2017 Guille Gonzalez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Markup.
+FOUNDATION_EXPORT double MarkupVersionNumber;
+
+//! Project version string for Markup.
+FOUNDATION_EXPORT const unsigned char MarkupVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Markup/PublicHeader.h>
+
+
