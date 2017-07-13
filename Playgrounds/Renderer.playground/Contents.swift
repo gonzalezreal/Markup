54c79897b7ec931eb72cb30038b8bfa57731f20e
Guille Gonzalez <gonzalezreal@icloud.com>
2017-07-13 09:49:28 +0200

    Remove extra tabs from sample text

--- Playgrounds/Renderer.playground/Contents.swift
+++ Playgrounds/Renderer.playground/Contents.swift
@@ -8,10 +8,12 @@ let label = UILabel()
 label.numberOfLines = 0
 label.backgroundColor = UIColor.white
 label.textColor = UIColor.black
-label.attributedText = renderer.render(text: """
+label.attributedText = renderer.render(text:
+	"""
 	The *quick*, ~red~ brown fox jumps over a _*lazy dog*_.
 	Compute _6*4*8_. _Quick_zephyrs_blow_.
-""")
+	"""
+)
 label.sizeToFit()
 
 PlaygroundPage.current.liveView = label
