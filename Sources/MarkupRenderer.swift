b07caf7a42270c66efa5333d294ce5ee8e696e19
Marcos Hernandez <marcos.hernandez@lyst.com>
2018-12-21 13:05:16 +0000

    fixed some intentations

--- Sources/MarkupRenderer.swift
+++ Sources/MarkupRenderer.swift
@@ -5,24 +5,24 @@
 //
 
 #if os(iOS) || os(tvOS) || os(watchOS)
-import UIKit.UIFont
-public typealias Font = UIFont
+	import UIKit.UIFont
+	public typealias Font = UIFont
 #elseif os(OSX)
-import AppKit.NSFont
-public typealias Font = NSFont
+	import AppKit.NSFont
+	public typealias Font = NSFont
 #endif
 
 public final class MarkupRenderer {
 	private let baseFont: Font
-	
+
 	public init(baseFont: Font) {
 		self.baseFont = baseFont
 	}
-	
+
 	public func render(text: String) -> NSAttributedString {
 		let elements = MarkupParser.parse(text: text)
 		let attributes = [NSAttributedString.Key.font: baseFont]
-		
+
 		return elements.map { $0.render(withAttributes: attributes) }.joined()
 	}
 }
@@ -32,21 +32,21 @@ private extension MarkupNode {
 		guard let currentFont = attributes[NSAttributedString.Key.font] as? Font else {
 			fatalError("Missing font attribute in \(attributes)")
 		}
-		
+
 		switch self {
 		case .text(let text):
 			return NSAttributedString(string: text, attributes: attributes)
-			
+
 		case .strong(let children):
 			var newAttributes = attributes
 			newAttributes[NSAttributedString.Key.font] = currentFont.boldFont()
 			return children.map { $0.render(withAttributes: newAttributes) }.joined()
-			
+
 		case .emphasis(let children):
 			var newAttributes = attributes
 			newAttributes[NSAttributedString.Key.font] = currentFont.italicFont()
 			return children.map { $0.render(withAttributes: newAttributes) }.joined()
-			
+
 		case .delete(let children):
 			var newAttributes = attributes
 			newAttributes[NSAttributedString.Key.strikethroughStyle] = NSUnderlineStyle.single.rawValue
@@ -67,32 +67,32 @@ extension Array where Element: NSAttributedString {
 }
 
 #if os(iOS) || os(tvOS) || os(watchOS)
-extension UIFont {
-	func boldFont() -> UIFont? {
-		return addingSymbolicTraits(.traitBold)
-	}
-	
-	func italicFont() -> UIFont? {
-		return addingSymbolicTraits(.traitItalic)
-	}
-	
-	func addingSymbolicTraits(_ traits: UIFontDescriptor.SymbolicTraits) -> UIFont? {
-		let newTraits = fontDescriptor.symbolicTraits.union(traits)
-		guard let descriptor = fontDescriptor.withSymbolicTraits(newTraits) else {
-			return nil
+	extension UIFont {
+		func boldFont() -> UIFont? {
+			return addingSymbolicTraits(.traitBold)
 		}
-		
-		return UIFont(descriptor: descriptor, size: 0)
+
+		func italicFont() -> UIFont? {
+			return addingSymbolicTraits(.traitItalic)
+		}
+
+		func addingSymbolicTraits(_ traits: UIFontDescriptor.SymbolicTraits) -> UIFont? {
+			let newTraits = fontDescriptor.symbolicTraits.union(traits)
+			guard let descriptor = fontDescriptor.withSymbolicTraits(newTraits) else {
+				return nil
+			}
+
+			return UIFont(descriptor: descriptor, size: 0)
+		}
 	}
-}
 #elseif os(OSX)
-extension NSFont {
-	func boldFont() -> NSFont? {
-		return NSFontManager.shared.convert(self, toHaveTrait: .boldFontMask)
+	extension NSFont {
+		func boldFont() -> NSFont? {
+			return NSFontManager.shared.convert(self, toHaveTrait: .boldFontMask)
+		}
+
+		func italicFont() -> NSFont? {
+			return NSFontManager.shared.convert(self, toHaveTrait: .italicFontMask)
+		}
 	}
-	
-	func italicFont() -> NSFont? {
-		return NSFontManager.shared.convert(self, toHaveTrait: .italicFontMask)
-	}
-}
 #endif
--- Tests/MarkupTests/MarkupRendererTests.swift
+++ Tests/MarkupTests/MarkupRendererTests.swift
@@ -20,7 +20,7 @@ class MarkupRendererTests: XCTestCase {
 		// given
 		let input = "hello there"
 		let expected = NSAttributedString(string: "hello there",
-										  attributes: [NSAttributedString.Key.font: baseFont])
+																			attributes: [NSAttributedString.Key.font: baseFont])
 		
 		// when
 		let result = sut.render(text: input)
@@ -35,36 +35,36 @@ class MarkupRendererTests: XCTestCase {
 		let boldFont = baseFont.boldFont()!
 		let expected = [
 			NSAttributedString(string: "hello ",
-							   attributes: [NSAttributedString.Key.font: baseFont]),
+												 attributes: [NSAttributedString.Key.font: baseFont]),
 			NSAttributedString(string: "there",
-							   attributes: [NSAttributedString.Key.font: boldFont])
+												 attributes: [NSAttributedString.Key.font: boldFont])
 			].joined()
-		
+
 		// when
 		let result = sut.render(text: input)
-		
+
 		// then
 		XCTAssertEqual(result, expected)
 	}
-	
+
 	func testEmphasizedText_render_rendersItalicText() {
 		// given
 		let input = "hello _there_"
 		let italicFont = baseFont.italicFont()!
 		let expected = [
 			NSAttributedString(string: "hello ",
-							   attributes: [NSAttributedString.Key.font: baseFont]),
+												 attributes: [NSAttributedString.Key.font: baseFont]),
 			NSAttributedString(string: "there",
-							   attributes: [NSAttributedString.Key.font: italicFont])
+												 attributes: [NSAttributedString.Key.font: italicFont])
 			].joined()
-		
+
 		// when
 		let result = sut.render(text: input)
-		
+
 		// then
 		XCTAssertEqual(result, expected)
 	}
-	
+
 	func testDeletedText_render_rendersStrikethroughText() {
 		// given
 		let input = "hello ~there~"
@@ -75,36 +75,36 @@ class MarkupRendererTests: XCTestCase {
 		]
 		let expected = [
 			NSAttributedString(string: "hello ",
-							   attributes: [NSAttributedString.Key.font: baseFont]),
+												 attributes: [NSAttributedString.Key.font: baseFont]),
 			NSAttributedString(string: "there",
-							   attributes: strikethroughAttributes)
+												 attributes: strikethroughAttributes)
 			].joined()
-		
+
 		// when
 		let result = sut.render(text: input)
-		
+
 		// then
 		XCTAssertEqual(result, expected)
 	}
-	
+
 	func testStrongEmphasizedText_render_rendersBoldItalicText() {
 		// given
 		let input = "hello *_there_* _*there*_"
 		let boldItalicFont = baseFont.boldFont()!.italicFont()!
 		let expected = [
 			NSAttributedString(string: "hello ",
-							   attributes: [NSAttributedString.Key.font: baseFont]),
+												 attributes: [NSAttributedString.Key.font: baseFont]),
 			NSAttributedString(string: "there",
-							   attributes: [NSAttributedString.Key.font: boldItalicFont]),
+												 attributes: [NSAttributedString.Key.font: boldItalicFont]),
 			NSAttributedString(string: " ",
-							   attributes: [NSAttributedString.Key.font: baseFont]),
+												 attributes: [NSAttributedString.Key.font: baseFont]),
 			NSAttributedString(string: "there",
-							   attributes: [NSAttributedString.Key.font: boldItalicFont])
+												 attributes: [NSAttributedString.Key.font: boldItalicFont])
 			].joined()
-		
+
 		// when
 		let result = sut.render(text: input)
-		
+
 		// then
 		XCTAssertEqual(result, expected)
 	}
